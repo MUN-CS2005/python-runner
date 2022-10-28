d1607ed97b40b9d9bd59ef2c82fafa356d1f5bd6
RuixL <slzjlrx@gmail.com>
2022-10-28 17:32:48 -0230

    Template Module

--- main/TemplateModule.py
+++ main/TemplateModule.py
@@ -15,8 +15,8 @@ class Template:
     This is the template module class.
     For more intuitive information, check TemplateModule.html.
     This module is not yet completed, everything depends on how the project goes.
-
-    :param app: receives the Flask() app object, currently necessary.
+    
+    :param app:receives the Flask() app object, currently necessary.
     :param page: receives a HTML page for template.
     :param title: receives a string to template {{title}} keyword.
     :param **placeholder: receives a dictionary of keyword for templating. {key: value}.
@@ -38,7 +38,7 @@ class Template:
                 'title': ch,
                 'attributes': self._placeholder
             }
-            return render_template(self._page, **attributes)
+            return render_template(self._page, **attributes, **self._placeholder)
         return
 
     def __run(self):
