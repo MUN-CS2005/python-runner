e832ae7ed268b671349bbc0d14ea29547ea6cf9a
JamesOlford <jsolford@mun.ca>
2022-11-03 17:22:21 -0230

    updated tests

--- main/setup.py
+++ main/setup.py
@@ -1,26 +1,15 @@
-"""
-The setup for team c's submission.
-uses setup tools
-"""
-
-
 from setuptools import setup, find_packages
 
 setup(
     name='codeServer',
     version='0.1.0',
-
+    description='A small coding based web application',
+    author='Team C',
     packages=find_packages(include=['main', 'main.*']),
-    # make sure required packages are installed
     install_requires=["sqlite3", "flask"],
     package_data={
-        '': ['*.txt', '*.pdf', '*.md']},
+        '': ['*.txt', '*.pdf']},
 
     setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
-
-    author='Team C',
-    description='A small coding based web application submitted for 2005 coursework',
-
-
+    tests_require=['pytest']
 )
