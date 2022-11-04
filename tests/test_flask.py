7128038bbad1aadcd2207c48877fdcbe1a8412a9
tkyDevs <98195386+tkyDevs@users.noreply.github.com>
2022-11-03 22:15:30 -0230

    mini issues with naming functions

--- tests/test_flask.py
+++ tests/test_flask.py
@@ -1,25 +1,25 @@
 """Tests for flask"""
 
 import unittest
-import requests
-import os
-from main import flaskServer
+from ..main import flaskServer
+
 
 class TestFlask(unittest.TestCase):
     """
         Unit test for flaskServer.py
     """
+
     def setUp(self):
         flaskServer.app.testing = True
         self.app = flaskServer.app.test_client()
 
-    def testResponse(self):
+    def test_response(self):
         rv = self.app.post('/run_code', data=dict(
-           codestuff='print(1)',
-       ), follow_redirects=True)
+            codestuff='print(1)',
+        ), follow_redirects=True)
         assert b'print(1)' in rv.data
         assert b'1' in rv.data
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     unittest.main()
