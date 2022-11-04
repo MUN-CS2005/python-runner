625cb0916649e16339f329ea78bc2b0361489dff
Ripu <86084706+singhripudaman@users.noreply.github.com>
2022-11-03 21:47:50 -0230

    Changed database.db location for better structure.

--- main/database/connection.py
+++ main/database/connection.py
@@ -3,7 +3,7 @@ import sqlite3
 
 class Connection:
     """A connection to the database"""
-    con = sqlite3.connect("database.db")
+    con = sqlite3.connect("../database.db")
 
     @classmethod
     def get_instance(cls) -> sqlite3.Connection:
