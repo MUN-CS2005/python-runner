0404c1e00901de40c9ce933392a1b42826a2cdc6
kytanaka <tkyDevs@gmail.com>
2022-11-13 20:44:25 -0330

    Refactored all html files. flaskServer.py is not done yet.

--- main/flaskServer.py
+++ main/flaskServer.py
@@ -7,81 +7,82 @@ Libraries used:
 By Kevin Tanaka(kytanaka - 202049565)
 Date: Nov, 2022
 """
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
 from subprocess import PIPE, STDOUT, run
+from database.user import User
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 app.secret_key = "testing"
 
+
+class regform(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=16)], render_kw={"placeholder": "Username:"})
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=16)], render_kw={"placeholder": "Password:"})
+    confirmation = PasswordField(validators=[InputRequired(), Length(min=4, max=16)], render_kw={"placeholder": "Repeat Password:"})
+    submit = SubmitField("Register")
+
+    def isusernamevalid(self, username, password, confirmation):
+        pass
+        #if username already in database:
+        #    raise ValidationError("Username already in use. Please try again.")
+        #elif password != confirmation:
+        #    raise ValidationError("Passwords do not match! Try again.")
+
+
+class loginform(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=16)],
+                                render_kw={"placeholder": "Username:"})
+    password = PasswordField(validators=[InputRequired(), Length(min=4, max=16)],
+                                 render_kw={"placeholder": "Password:"})
+    submit = SubmitField("Login")
+
+
+class changeform(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(min=4, max=16)],
+                           render_kw={"placeholder": "Username:"})
+    oldpw = PasswordField(validators=[InputRequired(), Length(min=4, max=16)],
+                             render_kw={"placeholder": "Old Password:"})
+    newpw = PasswordField(validators=[InputRequired(), Length(min=4, max=16)],
+                             render_kw={"placeholder": "New Password:"})
+    submit = SubmitField("Update")
+
 @app.route("/", methods=['POST', 'GET'])
-def login():
-    """Routing "/" as homepage when loading server"""
-    if request.method == "POST":
-        print("here")
-        user = request.form["username"]
-        passw = request.form["password"]
-        if len(user) == 0:
-            flash("Please input your username", "info")
-            return render_template("landingpage.html")
-        #elif user not in database:
-        #    flash("No such username in our database, please register!", "info")
-        #    return render_template("landingpage.html")
-        elif len(passw) == 0:
-            flash("Wrong password", "info")
-            return render_template("landingpage.html")
-        else:
-            print(f"/index/{user}")
-            return redirect(url_for((f"/index/{user}"), usr=user))
-    else:
-        return render_template("landingpage.html")
+def home():
+    form = loginform()
+    if form.validate_on_submit():
+        #if password i correct and user is in database
+        return redirect(url_for("index", usr=form.username.data))
+    return render_template("landingpage.html", form=form)
+
 
 @app.route("/<usr>", methods=['POST'])
-def client(usr):
-    #return f"<h1>{usr}</h1>"
-    return render_template("index.html")
-
-@app.route("/register", methods=['POST', 'GET'])
-def register():
-    if request.method == "POST":
-        username = request.form["username"]
-        pw = request.form["password"]
-        checkpw = request.form["check"]
-        print(username)
-        if len(username) == 0:
-            flash("Please input your username!", "info")
-            return redirect(url_for("register"))
-        #elif username already in database:
-        #    flash("This username is already claimed!", "info")
-        elif len(pw) == 0:
-            flash("Please choose a password!", "info")
-            return redirect(url_for("register"))
-        elif pw != checkpw:
-            flash("Passwords did not match! Please try again.", "info")
-            return redirect(url_for("register"))
-        else:
-            #Create user account in database
-            return render_template("landingpage.html")
-    else:
-        return render_template("register.html")
-
-@app.route("/changepw", methods=['POST', 'GET'])
-def resetpw():
-    return render_template("changepw.html")
-
-@app.route("/index/<usr>", methods=['POST', 'GET'])
 def index(usr):
-    #if request.method == "POST":
     test = render_template("index.html").replace("<!-- OUTPUT PLACEHOLDERNAMEOFTHEACCOUNTUSER -->", usr)
     return test
 
-@app.route("/run_code", methods=['POST'])
-def runcode():
-    """Routing the "/run_code" page"""
-    code = request.form['codestuff']
-    test = render_template("index.html").replace('ENTER CODE HERE', code)
-    p = run("python", stdout=PIPE, shell=True, stderr=STDOUT, input=code, encoding='ascii')
-    output = p.stdout
-    return test.replace('<!-- OUTPUT PLACEHOLDER -->', output)
+@app.route("/register", methods=['POST', 'GET'])
+def registration():
+    form = regform()
+
+    if form.validate_on_submit():
+        hash = bcrypt.generate_password_hash(form.password.data)
+        #User.create(username=form.username.data, password=hash)
+        return render_template("landingpage.html", form=form)
+    return render_template("register.html", form=form)
+
+
+@app.route("/changepw", methods=['POST', 'GET'])
+def updatepw():
+    form = changeform()
+    if form.validate_on_submit():
+        #return render_template("landingpage.html", form=form)
+        pass
+    return render_template("changepw.html", form=form)
 
 
 if __name__ == "__main__":
